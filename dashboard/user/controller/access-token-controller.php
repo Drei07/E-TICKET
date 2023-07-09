1f136de62f57e93c0e22e5e78d8029bbd7bcf7fb
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2023-07-09 13:25:13 +0800

    MAIN

--- dashboard/user/controller/access-token-controller.php
+++ dashboard/user/controller/access-token-controller.php
@@ -7,7 +7,7 @@ require __DIR__ . "/vendor/autoload.php";
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-class AccessToken
+class AccessTokenMandatory
 {
     private $conn;
 
@@ -268,23 +268,293 @@ class AccessToken
     }
 }
 
-// Add access tokens
+// Add access tokens Mandatory
 if (isset($_POST['btn-add-access-token-mandatory'])) {
     $user_id = $_GET["user_id"];
     $course_id = $_GET["course_id"];
     $year_level_id = $_GET["year_level_id"];
     $number_of_tokens = trim($_POST['access_token']);
 
-    $add_access_token = new AccessToken();
+    $add_access_token = new AccessTokenMandatory();
     $generated_tokens = $add_access_token->addAccessTokenMandatory($user_id, $course_id, $year_level_id, $number_of_tokens);
 }
 
-// Generate access tokens PDF and provide download link
+// Generate access tokens PDF and provide download link Mandatory
 if (isset($_GET['print_access_tokens-mandatory'])) {
     $course_id = $_GET["course_id"];
     $year_level_id = $_GET["year_level_id"];
 
-    $print_access_token = new AccessToken();
+    $print_access_token = new AccessTokenMandatory();
     $print_access_token->generateAccessTokensPDFMandatory($course_id, $year_level_id);
+}
+
+class AccessTokenOptional
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $database = new Database();
+        $db = $database->dbConnection();
+        $this->conn = $db;
+    }
+
+    public function addAccessTokenoOptional($user_id, $event_id, $number_of_tokens)
+    {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $tokenLength = 10;
+
+        $accessTokens = array();
+        $characterCount = strlen($characters);
+
+        while (count($accessTokens) < $number_of_tokens) {
+            $accessToken = '';
+            for ($j = 0; $j < $tokenLength; $j++) {
+                $randomIndex = mt_rand(0, $characterCount - 1);
+                $accessToken .= $characters[$randomIndex];
+            }
+
+            if (!$this->isTokenExistsOptional($accessToken)) {
+                $accessTokens[] = $accessToken;
+            }
+        }
+
+        // Insert the generated tokens into the database
+        $values = array();
+        $sql = 'INSERT INTO access_token (user_id, event_id, token) VALUES ';
+        foreach ($accessTokens as $token) {
+            $values[] = "(:user_id, :event_id, :token_$token)";
+        }
+        $sql .= implode(', ', $values);
+
+        $stmt = $this->runQuery($sql);
+        $stmt->bindValue(':user_id', $user_id);
+        $stmt->bindValue(':event_id', $event_id);
+
+
+        foreach ($accessTokens as $index => $token) {
+            $param = ":token_$token";
+            $stmt->bindValue($param, $token);
+        }
+
+        $exec = $stmt->execute();
+
+        if ($exec) {
+            $_SESSION['status_title'] = 'Success!';
+            $_SESSION['status'] = 'Successfully generated access tokens';
+            $_SESSION['status_code'] = 'success';
+            $_SESSION['status_timer'] = 40000;
+        } else {
+            $_SESSION['status_title'] = 'Oops!';
+            $_SESSION['status'] = 'Something went wrong, please try again!';
+            $_SESSION['status_code'] = 'error';
+            $_SESSION['status_timer'] = 100000;
+        }
+
+        header('Location: ../events-details');
+        exit();
+
+        return $accessTokens;
+    }
+
+    public function isTokenExistsOptional($accessToken)
+    {
+        $stmt = $this->runQuery('SELECT COUNT(*) FROM access_token WHERE token = :token');
+        $stmt->execute(array(':token' => $accessToken));
+        $count = $stmt->fetchColumn();
+
+        return ($count > 0);
+    }
+
+    public function generateAccessTokensPDFOptional($event_id)
+    {
+        //select course name
+        $stmt = $this->runQuery('SELECT * FROM events WHERE id = :id');
+        $stmt->execute(array(':id' => $event_id));
+        $event_data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Select access tokens with matching course, year, active status, and pending print_status
+        $stmt = $this->runQuery('SELECT * FROM access_token WHERE event_id = :event_id AND status = "active" AND print_status = "pending"');
+        $stmt->execute(array(':event_id' => $event_id));
+        $tokens = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (count($tokens) > 0) {
+            // Create the HTML content for the PDF
+            $html = '
+            <style>
+                .head {
+                    display: flex;
+                    justify-content: center;
+                }
+                .head img {
+                    width: 70px;
+                    height: 70px;
+                }
+                .head2 {
+                    display: block;
+                    text-align: center;
+                    padding: 10px;
+                }
+                .title {
+                    display: block;
+                    text-align: center;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                }
+                th, td {
+                    border: 1px solid black;
+                    padding: 5px;
+                    text-align: center;
+                }
+            </style>
+            <div class="head">
+                <div class="head2">
+                    <h1>DOMINICAN COLLEGE OF TARLAC</h1>
+                    <p>Mac Arthur Highway, Poblacion (Sto.Cristo), Capas. 2315 Tarlac, Philippinnes</p>
+                    <p>Tel.No. (045) 491-7578/Telefax (045) 925-0519</p>
+                    <p>E-mail: domct2315@yahoo.com</p>
+                </div>
+            </div>
+            <div class="title">
+            <h2>' . $event_data['event_name'] . '</h2>
+                <h4>OPTIONAL EVENT ACCESS TOKENS</h4>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Access Tokens</th>
+                        <th>Date Printed</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+            $number = 1;
+            foreach ($tokens as $token) {
+                $datePrinted = date('m/d/y (h:i:s A)', time());
+                $html .= '
+                    <tr>
+                        <td>' . $number . '</td>
+                        <td>' . $token['token'] . '</td>
+                        <td>' . $datePrinted . '</td>
+                    </tr>';
+                $number++;
+            }
+
+            $html .= '
+                </tbody>
+            </table>
+            ';
+
+
+
+            // Create a new Dompdf instance
+            $options = new Options();
+            $options->set('isRemoteEnabled', true);
+            $dompdf = new Dompdf($options);
+            $dompdf->loadHtml($html);
+
+            // Render the HTML content to PDF
+            $dompdf->render();
+
+            // Save the PDF file
+            $event_name = str_replace(' ', '_', $event_data['event_name']);
+            $timestamp = time();
+            $filename = ''.$event_name.'_access_tokens_'.$timestamp.'.pdf';
+            $output = $dompdf->output();
+            file_put_contents($filename, $output);
+
+            // Specify the backup folder path
+            $backupFolderPath = '../../pdf/';
+
+            // Move the PDF file to the backup folder
+            $backupFilename = $backupFolderPath . $filename;
+            rename($filename, $backupFilename);
+
+            // Update the print_status of the generated tokens
+            $tokenIds = array_column($tokens, 'id');
+            $this->updatePrintStatusOptional($tokenIds);
+
+            // Save the PDF filename in the database
+            $this->savePdfFilenameToDatabaseOptional($event_id, $filename);
+
+            // Provide the link to download the PDF file
+            if (file_exists($backupFilename)) {
+                // Download the PDF file
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment;filename=' . basename($backupFilename));
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($backupFilename));
+                ob_clean();
+                flush();
+            
+                // Read the file and output its contents
+                readfile($backupFilename);
+        
+                exit();
+            } else {
+                $_SESSION['status_title'] = 'Oops!';
+                $_SESSION['status'] = 'Something went wrong, please try again!';
+                $_SESSION['status_code'] = 'error';
+                $_SESSION['status_timer'] = 100000;
+                header('Location: ../events-details');
+                exit();
+            }
+        } else {
+            $_SESSION['status_title'] = 'Oops!';
+            $_SESSION['status'] = 'Access token are printed already, try to generate again thank you!';
+            $_SESSION['status_code'] = 'error';
+            $_SESSION['status_timer'] = 100000;
+            header('Location: ../events-details');
+            exit();
+        }
+    }
+
+    public function updatePrintStatusOptional($tokenIds)
+    {
+        // Update the print_status of the given token IDs to "printed"
+        $placeholders = implode(',', array_fill(0, count($tokenIds), '?'));
+        $sql = "UPDATE access_token SET print_status = 'printed' WHERE id IN ($placeholders)";
+        $stmt = $this->runQuery($sql);
+        $stmt->execute($tokenIds);
+    }
+
+    public function savePdfFilenameToDatabaseOptional($event_id, $filename)
+    {
+        $stmt = $this->runQuery('INSERT INTO pdf_file (event_id, file_name) VALUES (:event_id, :file_name)');
+        $exec = $stmt->execute(array(
+            ":event_id"  => $event_id,
+            ":file_name"         => $filename,
+        ));
+    
+    }
+
+
+    public function runQuery($sql)
+    {
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
+    }
+}
+
+// Add access tokens Optional
+if (isset($_POST['btn-add-access-token-optional'])) {
+    $user_id = $_GET["user_id"];
+    $event_id = $_GET["event_id"];
+    $number_of_tokens = trim($_POST['access_token']);
+
+    $add_access_token = new AccessTokenOptional();
+    $generated_tokens = $add_access_token->addAccessTokenoOptional($user_id, $event_id, $number_of_tokens);
+}
+
+// Generate access tokens PDF and provide download link Optional
+if (isset($_GET['print_access_tokens_optional'])) {
+    $event_id = $_GET["event_id"];
+
+    $print_access_token = new AccessTokenOptional();
+    $print_access_token->generateAccessTokensPDFOptional($event_id);
 }
 ?>
--- dashboard/user/index.php
+++ dashboard/user/index.php
@@ -3,39 +3,43 @@ include_once 'header.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php
-    include_once '../../configuration/header.php';
-    ?>
+	<?php
+	include_once '../../configuration/header.php';
+	?>
 	<title>Dashboard</title>
 </head>
+
 <body>
 
-<!-- Loader -->
-<div class="loader"></div>
+	<!-- Loader -->
+	<div class="loader"></div>
 
 	<!-- SIDEBAR -->
 	<section id="sidebar">
 		<a href="" class="brand">
 			<img src="../../src/img/<?php echo $config->getSystemLogo() ?>" alt="logo">
-			<span class="text">DOMINICAN<br><p>COLLEGE OF TARLAC</p></span>
+			<span class="text">DOMINICAN<br>
+				<p>COLLEGE OF TARLAC</p>
+			</span>
 		</a>
 		<ul class="side-menu top">
-			<li  class="active">
+			<li class="active">
 				<a href="./">
-					<i class='bx bxs-dashboard' ></i>
+					<i class='bx bxs-dashboard'></i>
 					<span class="text">Dashboard</span>
 				</a>
 			</li>
 			<li>
 				<a href="events">
-					<i class='bx bxs-calendar' ></i>
+					<i class='bx bxs-calendar'></i>
 					<span class="text">Events</span>
 				</a>
 			</li>
 			<li>
 				<a href="access-tokens">
-					<i class='bx bxs-key' ></i>
+					<i class='bx bxs-key'></i>
 					<span class="text">Access Tokens</span>
 				</a>
 			</li>
@@ -43,7 +47,7 @@ include_once 'header.php';
 		<ul class="side-menu top">
 			<li>
 				<a href="authentication/user-signout" class="btn-signout">
-					<i class='bx bxs-log-out-circle' ></i>
+					<i class='bx bxs-log-out-circle'></i>
 					<span class="text">Signout</span>
 				</a>
 			</li>
@@ -57,16 +61,16 @@ include_once 'header.php';
 	<section id="content">
 		<!-- NAVBAR -->
 		<nav>
-			<i class='bx bx-menu' ></i>
+			<i class='bx bx-menu'></i>
 			<form action="#">
 				<div class="form-input">
 					<input type="search" placeholder="Search...">
-					<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+					<button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
 				</div>
 			</form>
 			<div class="username">
-                <span>Hello, <label for=""><?php echo $user_fname ?></label></span>
-            </div>
+				<span>Hello, <label for=""><?php echo $user_fname ?></label></span>
+			</div>
 			<a href="profile" class="profile" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Profile">
 				<img src="../../src/img/<?php echo $user_profile ?>">
 			</a>
@@ -92,146 +96,64 @@ include_once 'header.php';
 
 			<ul class="dashboard_data">
 				<li>
-					<i class='bx bx-user-circle'></i>
+					<i class='bx bxs-calendar'></i>
 					<span class="text">
-					<?php
-								$pdoQuery = "SELECT * FROM 	users WHERE user_type = :user_type";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute(array(":user_type" => 2));
+						<?php
+						$pdoQuery = "SELECT COUNT(*) AS total_count FROM events WHERE course_id IN (SELECT id FROM course WHERE department_id = :department_id)";
+						$pdoResult = $pdoConnect->prepare($pdoQuery);
+						$pdoResult->execute(array(":department_id" => $user_department));
+						$row = $pdoResult->fetch(PDO::FETCH_ASSOC);
+						$total_count = $row['total_count'];
 
-								$count = $pdoResult1->rowCount();
-
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
-						<p>Sub-admin</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-buildings' ></i>
-					<span class="text">
-							<?php
-								$pdoQuery = "SELECT * FROM department";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute();
-
-								$count = $pdoResult1->rowCount();
-
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
-						<p>Department</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-book' ></i>
-					<span class="text">
-							<?php
-								$pdoQuery = "SELECT * FROM course";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute();
-
-								$count = $pdoResult1->rowCount();
-
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
-						<p>Course</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-graduation'></i>
-					<span class="text">
-					<?php
-								$pdoQuery = "SELECT * FROM  year_level";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute();
-
-								$count = $pdoResult1->rowCount();
-
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
-						<p>Year Level</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-calendar' ></i>
-					<span class="text">
-							<?php
-								$pdoQuery = "SELECT * FROM events";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute();
-
-								$count = $pdoResult1->rowCount();
-
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
+						echo "<h3>$total_count</h3>";
+						?>
 						<p>Events</p>
 					</span>
 				</li>
 				<li>
 					<i class='bx bxs-calendar-event'></i>
 					<span class="text">
-					<?php
-								$pdoQuery = "SELECT * FROM  course_event";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute();
+						<?php
+						$pdoQuery = "SELECT COUNT(*) AS total_count FROM course_event WHERE status = :status AND course_id IN (SELECT id FROM course WHERE department_id = :department_id)";
+						$pdoResult = $pdoConnect->prepare($pdoQuery);
+						$pdoResult->execute(array(":status" => "active", ":department_id" => $user_department));
+						$row = $pdoResult->fetch(PDO::FETCH_ASSOC);
+						$total_count = $row['total_count'];
 
-								$count = $pdoResult1->rowCount();
-
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
+						echo "<h3>$total_count</h3>";
+						?>
 						<p>Course Events</p>
 					</span>
 				</li>
 				<li>
 					<i class='bx bx-calendar'></i>
 					<span class="text">
-					<?php
-								$pdoQuery = "SELECT * FROM 	events WHERE event_type = :event_type";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute(array(":event_type" => "MANDATORY"));
+						<?php
+						$pdoQuery = "SELECT COUNT(*) AS total_count FROM events WHERE course_id IN (SELECT id FROM course WHERE department_id = :department_id) AND event_type = :event_type";
+						$pdoResult = $pdoConnect->prepare($pdoQuery);
+						$pdoResult->execute(array(":department_id" => $user_department, ":event_type" => "MANDATORY"));
+						$row = $pdoResult->fetch(PDO::FETCH_ASSOC);
+						$total_count = $row['total_count'];
 
-								$count = $pdoResult1->rowCount();
+						echo "<h3>$total_count</h3>";
+						?>
 
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
 						<p>Mandatory Events</p>
 					</span>
 				</li>
 				<li>
 					<i class='bx bx-calendar'></i>
 					<span class="text">
-					<?php
-								$pdoQuery = "SELECT * FROM 	events WHERE event_type = :event_type";
-								$pdoResult1 = $pdoConnect->prepare($pdoQuery);
-								$pdoResult1->execute(array(":event_type" => "OPTIONAL"));
+						<?php
+						$pdoQuery = "SELECT COUNT(*) AS total_count FROM events WHERE course_id IN (SELECT id FROM course WHERE department_id = :department_id) AND event_type = :event_type";
+						$pdoResult = $pdoConnect->prepare($pdoQuery);
+						$pdoResult->execute(array(":department_id" => $user_department, ":event_type" => "OPTIONAL"));
+						$row = $pdoResult->fetch(PDO::FETCH_ASSOC);
+						$total_count = $row['total_count'];
 
-								$count = $pdoResult1->rowCount();
+						echo "<h3>$total_count</h3>";
+						?>
 
-								echo
-								"
-									<h3>$count</h3>
-								";
-							?>
 						<p>Optional Events</p>
 					</span>
 				</li>
@@ -242,27 +164,27 @@ include_once 'header.php';
 	<!-- CONTENT -->
 
 	<?php
-    include_once '../../configuration/footer.php';
-    ?>
+	include_once '../../configuration/footer.php';
+	?>
 
-		<!-- SWEET ALERT -->
-		<?php
+	<!-- SWEET ALERT -->
+	<?php
 
-		if(isset($_SESSION['status']) && $_SESSION['status'] !='')
-		{
-		?>
+	if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
+	?>
 		<script>
 			swal({
-			title: "<?php echo $_SESSION['status_title']; ?>",
-			text: "<?php echo $_SESSION['status']; ?>",
-			icon: "<?php echo $_SESSION['status_code']; ?>",
-			button: false,
-			timer: <?php echo $_SESSION['status_timer']; ?>,
+				title: "<?php echo $_SESSION['status_title']; ?>",
+				text: "<?php echo $_SESSION['status']; ?>",
+				icon: "<?php echo $_SESSION['status_code']; ?>",
+				button: false,
+				timer: <?php echo $_SESSION['status_timer']; ?>,
 			});
 		</script>
-		<?php
+	<?php
 		unset($_SESSION['status']);
-		}
-		?>
+	}
+	?>
 </body>
+
 </html>
